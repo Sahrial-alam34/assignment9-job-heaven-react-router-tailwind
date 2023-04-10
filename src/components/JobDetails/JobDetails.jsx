c5971abd2d266f24586bffddf8f413bd94677391
Sahrial-alam34 <sunnykcp01@gmail.com>
2023-04-10 18:13:51 +0600

    update job details page

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -29,13 +29,51 @@ const JobDetails = () => {
             </div>
             <div className='container text-left flex gap-10 '>
                 <div className='bg-slate-100 flex-auto w-[70%] p-10'>
-                    <p><span className='font-bold'>Job Description:</span> <small>{jobInfo.jobDescription}</small></p>
-                    <p><span className='font-bold'>Job Responsibility:</span> <small>{jobInfo.jobResponsibility}</small></p>
-                    <p><span className='font-bold'>Educational Requirements::</span> <small>{jobInfo.educationRequirements}</small></p>
-                    <p><span className='font-bold'>Experiences:</span> <small>{jobInfo.exprience}</small></p>
+                    <p><span className='font-bold '>Job Description:</span> <small>{jobInfo.jobDescription}</small></p>
+                    <p className='mt-5'><span className='font-bold'>Job Responsibility:</span> <small>{jobInfo.jobResponsibility}</small></p>
+                    <p className='mt-5'><span className='font-bold'>Educational Requirements::</span> <small>{jobInfo.educationRequirements}</small></p>
+                    <p className='mt-5'><span className='font-bold'>Experiences:</span> <small>{jobInfo.exprience}</small></p>
                 </div>
-                <div className=' bg-slate-400  flex-auto w-[30%]'>
-                    <h2>Title: {jobInfo.title}</h2>
+                <div className=' bg-cyan-100  flex-auto w-[30%] p-10'>
+                    <div>
+                        <p className='font-bold mb-5'>Job Details</p>
+                        <hr  className='border-solid border-cyan-700' />
+                        <div>
+                            <div className='flex gap-3 mt-4'>
+                                <img src="/src/assets/Icons/Frame.png" alt="" />
+                                <p><span className='font-bold'>Salary:</span> <small>{jobInfo.salary}</small></p>
+
+                            </div>
+                            <div className='flex gap-3 mt-4'>
+                                <img src="/src/assets/Icons/Frame-1.png" alt="" />
+                                <p><span className='font-bold'>Job Title:</span> <small>{jobInfo.title}</small></p>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='mt-10'>
+                        <p className='font-bold mb-5'>Contact Information</p>
+                        <hr  className='border-solid border-cyan-700' />
+                        <div>
+                            <div className='flex gap-3 mt-4'>
+                                <img src="/src/assets/Icons/Frame-2.png" alt="" />
+                                <p><span className='font-bold'>Phone:</span> <small>{jobInfo.phone}</small></p>
+
+                            </div>
+                            <div className='flex gap-3 mt-4'>
+                                <img src="/src/assets/Icons/Frame-3.png" alt="" />
+                                <p><span className='font-bold'>Email:</span> <small>{jobInfo.email}</small></p>
+
+                            </div>
+                            <div className='flex gap-3 mt-4'>
+                                <img src="/src/assets/Icons/Frame-4.png" alt="" />
+                                <p><span className='font-bold'>Address:</span> <small>{jobInfo.address}</small></p>
+
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
